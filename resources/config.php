cf4ad3596b32c8fcadfd7c7b481de59dc7073552
U-Namai-PC\Namai <geraskalnas@gmail.com>
2019-04-20 19:07:59 +0300

    Update Path

--- resources/config.php
+++ resources/config.php
@@ -27,7 +27,7 @@ $config = array(
         "baseUrl" => "http://example.com"
     ),
     "paths" => array(
-        "resources" => "/path/to/resources",
+        "resources" => "./",
         "images" => array(
             "content" => $_SERVER["DOCUMENT_ROOT"] . "/images/content",
             "layout" => $_SERVER["DOCUMENT_ROOT"] . "/images/layout"
