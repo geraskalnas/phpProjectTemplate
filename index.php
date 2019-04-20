0a0e4a228d0f2ba19702f63f0b25e7f8b8e570c0
geraskalnas <geraskalnas@gmail.com>
2019-04-20 19:13:45 +0300

    Fix config.php path

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
  
-    require_once(realpath(dirname(__FILE__) . "/../resources/config.php"));
+    require_once(resources/config.php");
  
     require_once(LIBRARY_PATH . "/templateFunctions.php");
  
