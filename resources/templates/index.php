5337893d5a17d44fd9cc2430aad3ffb551b4cbd6
U-Namai-PC\Namai <geraskalnas@gmail.com>
2019-04-20 18:34:10 +0300

    Updated index page

--- resources/templates/index.php
+++ resources/templates/index.php
@@ -1,17 +1,21 @@
-<?php    
-    // load up your config file
-    require_once("/path/to/resources/config.php");
-     
-    require_once(TEMPLATES_PATH . "/header.php");
-?>
-<div id="container">
-    <div id="content">
-        <!-- content -->
-    </div>
-    <?php
-        require_once(TEMPLATES_PATH . "/rightPanel.php");
-    ?>
-</div>
 <?php
-    require_once(TEMPLATES_PATH . "/footer.php");
+ 
+    require_once(realpath(dirname(__FILE__) . "/../resources/config.php"));
+ 
+    require_once(LIBRARY_PATH . "/templateFunctions.php");
+ 
+    /*
+        Now you can handle all your php logic outside of the template
+        file which makes for very clean code!
+    */
+     
+    $setInIndexDotPhp = "Hey! I was set in the index.php file.";
+     
+    // Must pass in variables (as an array) to use in template
+    $variables = array(
+        'setInIndexDotPhp' => $setInIndexDotPhp
+    );
+     
+    renderLayoutWithContentFile("home.php", $variables);
+ 
 ?>
